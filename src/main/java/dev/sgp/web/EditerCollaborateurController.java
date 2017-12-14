90314fb4267780eb8770e968b766a948e3d7e40d
AlexGeb <alex.behaghel@gmail.com>
2017-12-14 17:46:45 +0100

    ajout d'un logger

--- src/main/java/dev/sgp/web/EditerCollaborateurController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -1,17 +1,17 @@
 package dev.sgp.web;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import dev.sgp.entite.Collaborateur;
 import dev.sgp.service.CollaborateurService;
@@ -20,15 +20,18 @@ import dev.sgp.util.FormValidator;
 
 public class EditerCollaborateurController extends HttpServlet {
 	private CollaborateurService collabService = Constantes.COLLAB_SERVICE;
+	private static final Logger LOG = LoggerFactory.getLogger(EditerCollaborateurController.class);
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String matricule = req.getParameter("matricule");
 		Collaborateur collab = collabService.queryByMatricule(matricule).orElse(null);
 		if (collab == null || matricule == null) {
+			LOG.debug("trying to get an unknown matricule");
 			resp.sendError(400, "Matricule inconnu ou non spécifié");
 		} else {
 			req.setAttribute("collaborateur", collab);
+			LOG.debug("Modification du collaborateur " + collab.getPrenom() + " " + collab.getNom());
 			req.getRequestDispatcher("/WEB-INF/views/collab/editerCollaborateur.jsp").forward(req, resp);
 		}
 	}
@@ -41,6 +44,7 @@ public class EditerCollaborateurController extends HttpServlet {
 		if (itemManquants.isEmpty()) {
 			String matricule = req.getParameter("matricule");
 			collabService.modifyCollaborateur(matricule, keyValue);
+			LOG.debug("Modification réussie");
 			resp.setStatus(201);
 			resp.sendRedirect(req.getContextPath() + "/collaborateurs/lister");
 		} else {
